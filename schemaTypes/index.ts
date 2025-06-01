623d1e0177c0ab6c8b0778f30d86f3aa637a5d64
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2025-06-01 20:19:20 +0200

    add validations to schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,5 @@
+import {artistType} from './artist-type'
 import {eventType} from './event-type'
+import {venueType} from './venue-type'
 
-export const schemaTypes = [eventType]
+export const schemaTypes = [eventType, artistType, venueType]
